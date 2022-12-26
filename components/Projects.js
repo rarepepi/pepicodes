bbd0d128bae61b7d326535c21301eb340850922e
rarepepi <pepimartinez@protonmail.com>
2022-12-26 02:42:09 -0500

    www

--- components/Projects.js
+++ components/Projects.js
@@ -141,7 +141,7 @@ const Projects = () => {
         </Slide>
         <Slide right>
           <div className="hover:bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-            <a href="https://hootproject.com" target="_blank">
+            <a href="https://www.hootproject.com" target="_blank">
               <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
                 <h2 className="text-white text-center font-bold md:text-2xl text-lg">
                   🦉
