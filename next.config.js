9ba0f1558a0bb6ca9c42c8158e88912701190163
rarepepi <pepimartinez@protonmail.com>
2023-04-21 02:48:35 -0400

    new next js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-require("dotenv").config();
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-  },
-};
+}
+
+module.exports = nextConfig
