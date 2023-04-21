2a4bbb9bf1bcfe1be0ccdf97f9b4ed0a9c2bc90c
rarepepi <pepimartinez@protonmail.com>
2023-04-21 02:49:01 -0400

    new tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,63 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media", // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    backgroundColor: theme => ({
-      'primary': '#34d399',
-      'secondary': '#8a5cf6',
-      'danger': '#e3342f',
-      'background': '#0a192f',
-      'bg-light': '#112240'
+    backgroundColor: (theme) => ({
+      primary: "#34d399",
+      secondary: "#8a5cf6",
+      danger: "#e3342f",
+      background: "#0a192f",
+      "bg-light": "#112240",
     }),
     extend: {
       backgroundImage: (theme) => ({
         "bg-header": "url('/img/bg-header.png')",
-        "hoot": "url('/img/hoot.png')",
+        hoot: "url('/img/hoot.png')",
       }),
       zIndex: {
-
-        '-10': '-10',
+        "-10": "-10",
       },
       screens: {
-        'sm': '640px',
+        sm: "640px",
         // => @media (min-width: 640px) { ... }
 
-        'md': '768px',
+        md: "768px",
         // => @media (min-width: 768px) { ... }
 
-        'lg': '1024px',
+        lg: "1024px",
         // => @media (min-width: 1024px) { ... }
 
-        'xl': '1280px',
+        xl: "1280px",
         // => @media (min-width: 1280px) { ... }
 
-        '2xl': '1536px',
+        "2xl": "1536px",
         // => @media (min-width: 1536px) { ... }
-      }
+      },
     },
     fontFamily: {
-      'poppins': ['Poppins', 'sans-serif']
-    }
-  },
-  variants: {
-    extend: {},
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('tailwindcss-line-length')({
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("tailwindcss-line-length")({
       widths: {
-        'xs': '18em', // 300px at 1rem
-        'sm': '30em',
-        'md': '35em', // 560px at 1rem
-        'lg': '40em',
-        'xl': '50em', // 800px at 1rem
+        xs: "18em", // 300px at 1rem
+        sm: "30em",
+        md: "35em", // 560px at 1rem
+        lg: "40em",
+        xl: "50em", // 800px at 1rem
       },
-      variants: ['responsive'],
+      variants: ["responsive"],
     }),
   ],
 };
