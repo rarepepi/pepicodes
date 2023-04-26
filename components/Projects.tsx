5332e005ee236b2b6a3bc488d7b30003a658b950
rarepepi <pepimartinez@protonmail.com>
2023-04-26 03:50:24 -0400

    use an array for projects and refactor

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,263 +1,124 @@
-import { Flip, Slide } from "react-awesome-reveal";
+import { Zoom, Fade } from "react-awesome-reveal";
 
 const Projects = () => {
+  const projects = [
+    {
+      title: "biorxiv.ai",
+      description: "The AI Professor for Biology",
+      link: "https://biorxiv.ai",
+      icon: "🧬",
+    },
+    {
+      title: "medrxiv.ai",
+      description: "The AI Professor for Medicine",
+      link: "https://medrxiv.ai",
+      icon: "⚕️",
+    },
+    {
+      title: "Language.help",
+      description: "A better translator than Google, using GPT-3.5",
+      link: "https://language.help",
+      icon: "🗺️",
+    },
+    {
+      title: "Art Gobblers Gallery Assistant",
+      description: "A microservice that tracks art gobbles and glams.",
+      link: "https://github.com/rarepepi/artgobblers-discord-bot",
+      icon: "🖼️",
+    },
+    {
+      title: "Summify.ai",
+      description: "Summarizing your favorite discord.",
+      link: "https://summify.ai",
+      icon: "📝",
+    },
+    {
+      title: "Blockery",
+      description: "Your new home of NFT Analytics and Knowledge.",
+      link: "https://blockery.app",
+      icon: "📈",
+    },
+    {
+      title: "MargsGo",
+      description: "A revolutionary parody startup",
+      link: "https://marsgo.xyz/",
+      icon: "🪐",
+    },
+    {
+      title: "Alpha Explorers",
+      description: "The most powerful tools in web3 all at your fingertips.",
+      link: "https://alpha-explorer.vercel.app",
+      icon: "✨",
+    },
+    {
+      title: "Building Beaverz",
+      description: "A team based NFT game.",
+      link: "https://busy-beaverz.vercel.app",
+      icon: "🦫",
+    },
+    {
+      title: "WutFloor",
+      description: "An NFT portfolio analyzer.",
+      link: "https://wutfloor.xyz",
+      icon: "📊",
+    },
+    {
+      title: "Hoot",
+      description: "A fully on-chain collection of CC0 MoonBirds.",
+      link: "https://www.hootproject.com",
+      icon: "🦉",
+    },
+    {
+      title: "Colors On Chain",
+      description: "A fun game about colors and NFT art.",
+      link: "https://colorsonchain.com",
+      icon: "🎨",
+    },
+    {
+      title: "Wall Street Bets Scraper",
+      description: "Count occurances of tickers on WSB subreddit.",
+      link: "https://github.com/rarepepi/wsbscraper",
+      icon: "🧮",
+    },
+    {
+      title: "Alfred",
+      description:
+        "An stock account and crypto account aggregator. Works through telegram.",
+      link: "https://github.com/rarepepi/alfred",
+      icon: "🤖",
+    },
+  ];
+
   return (
     <div id="PROJECTS">
-      <Flip>
+      <Zoom>
         <div>
           <h2 className="text-gray-300 md:text-2xl text-lg text-center font-bold">
             <span className="text-green-300 mr-4">02.</span>Some Things
             I&apos;ve Built
           </h2>
         </div>
-      </Flip>
+      </Zoom>
       <div className="flex flex-row flex-wrap justify-center md:p-16 ">
-        <div></div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a href="https://language.help" target="_blank" rel="noreferrer">
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🗺️
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Language.help
-                </h2>
+        {projects.map((project) => (
+          <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
+            <Fade>
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96 min-w">
+                  <h2 className="text-white text-center font-bold md:text-2xl text-lg">
+                    {project.icon}
+                  </h2>
 
-                <p className="p-8 w-max-lg ">
-                  A better translator than Google, using GPT-3.5
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://github.com/rarepepi/artgobblers-discord-bot"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🖼️
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Art Gobblers Gallery Assistant
-                </h2>
+                  <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
+                    {project.title}
+                  </h2>
 
-                <p className="p-8 w-max-lg ">
-                  A microservice that tracks art gobbles and glams.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a href="https://summify.ai" target="_blank" rel="noreferrer">
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  📝
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Summify.ai
-                </h2>
-
-                <p className="p-8 w-max-lg ">
-                  Summarizing your favorite discord.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a href="https://blockery.app" target="_blank" rel="noreferrer">
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  📈
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Blockery
-                </h2>
-
-                <p className="p-8 w-max-lg ">
-                  Your new home of NFT Analytics and Knowledge.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://alpha-explorer.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  ✨
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Alpha Explorers
-                </h2>
-
-                <p className="p-8 w-max-lg ">
-                  The most powerful tools in web3 all at your fingertips.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://busy-beaverz.vercel.app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110  max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🦫
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Building Beaverz
-                </h2>
-
-                <p className="p-8 w-max-lg ">A team based NFT game.</p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a href="https://wutfloor.xyz" target="_blank" rel="noreferrer">
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  📊
-                </h2>
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  WutFloor
-                </h2>
-
-                <p className="p-8  w-max-lg">An NFT portfolio analyzer.</p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://www.hootproject.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🦉
-                </h2>
-
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Hoot Project
-                </h2>
-
-                <p className="p-8 w-max-lg">
-                  Hoot is a fully on-chain collection of CC0 MoonBirds.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a href="https://numbergame.io" target="_blank" rel="noreferrer">
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  #️⃣
-                </h2>
-
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Number Game
-                </h2>
-
-                <p className="p-8 w-max-lg">A math challenge through NFTs.</p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://colorsonchain.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🎨
-                </h2>
-
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Colors On Chain
-                </h2>
-
-                <p className="p-8 w-max-lg">
-                  A fun game about colors and NFT art.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://github.com/rarepepi/wsbscraper"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🧮
-                </h2>
-
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Wall Street Bets Scraper
-                </h2>
-
-                <p className="p-8 w-max-lg">
-                  Count occurances of tickers on WSB subreddit.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
-        <div className="bg-bg-light m-4 text-center hover:ring-2 hover:ring-green-300 md:w-1/3 cursor-pointer flex justify-center flex-row flex-wrap  py-8  md:space-x-12">
-          <Slide>
-            <a
-              href="https://github.com/rarepepi/alfred"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="text-white flex flex-col justify-center rounded-lg p-4 motion-safe:hover:scale-110 max-w-lg w-96 min-w">
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg">
-                  🤖
-                </h2>
-
-                <h2 className="text-white text-center font-bold md:text-2xl text-lg mt-6">
-                  Alfred
-                </h2>
-
-                <p className="p-8 w-max-lg">
-                  An stock account and crypto account aggregator. Works through
-                  telegram.
-                </p>
-              </div>
-            </a>
-          </Slide>
-        </div>
+                  <p className="p-8 w-max-lg">{project.description}</p>
+                </div>
+              </a>
+            </Fade>
+          </div>
+        ))}
       </div>
       <style jsx>{``}</style>
     </div>
