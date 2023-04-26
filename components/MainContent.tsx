074f423c9d723de26ee32ec4f111570e65bc613f
rarepepi <pepimartinez@protonmail.com>
2023-04-26 16:14:23 -0400

    fix multi render

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -4,18 +4,18 @@ const MainContent = () => {
   return (
     <div id="Why" className="">
       <div className="flex flex-col justify-left mt-16 md:p-16 md:ml-32 h-screen">
-        <Fade delay={250}>
+        <Fade triggerOnce delay={250}>
           <p className="text-green-300 text-lg">Hi, my name is</p>
           <h1 className="xl:text-6xl text-5xl font-bold text-gray-200 mt-8">
             Pepi
           </h1>
         </Fade>
-        <Fade delay={500}>
+        <Fade triggerOnce delay={500}>
           <h2 className="text-gray-300 xl:text-6xl text-5xl mt-4 font-bold">
             I like to build things.
           </h2>
         </Fade>
-        <Fade delay={750}>
+        <Fade triggerOnce delay={750}>
           <h3 className="text-gray-400  mt-4 md:w-1/2">
             I&#39;m a software engineer passionate about decentrailized
             applications. My current focus includes smart contracts,
@@ -25,7 +25,7 @@ const MainContent = () => {
         </Fade>
 
         <div className="flex flex-row flex-wrap justify-left mt-8 sm:space-x-6 sm:space-y-0 space-y-4">
-          <Fade delay={1000}>
+          <Fade triggerOnce delay={1000}>
             <a
               className=" p-3 motion-safe:hover:scale-110 hover:bg-primary hover:text-black flex text-green-300 ring-green-300 ring-2 "
               href="https://pepi.codes/resume.pdf"
