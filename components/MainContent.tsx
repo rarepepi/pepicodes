26187fcd771c52b79a5e5aaa1ebfcbbf43ed7332
rarepepi <pepimartinez@protonmail.com>
2023-05-03 15:12:00 -0400

    fix main content animation

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -4,7 +4,7 @@ const MainContent = () => {
   return (
     <div id="Why" className="">
       <div className="flex flex-col justify-left mt-16 md:p-16 md:ml-32 h-screen">
-        <Fade delay={250} cascade triggerOnce>
+        <Fade delay={250} triggerOnce>
           <p className="text-green-300 text-lg">Hi, my name is</p>
           <h1 className="xl:text-6xl text-5xl font-bold text-gray-200 mt-8">
             Pepi
@@ -14,23 +14,21 @@ const MainContent = () => {
             I like to build things.
           </h2>
 
-          <h3 className="text-gray-400  mt-4 md:w-1/2">
+          <h3 className="text-gray-400  my-8 md:w-1/2">
             I&#39;m a software engineer passionate about decentralized
             applications. My current focus includes smart contracts,
             <span className="text-green-300"> NFTs, DAOs, and DeFi, </span>
             as well as any innovative projects that pique my curiosity.
           </h3>
 
-          <div className="flex flex-row flex-wrap justify-left mt-8 sm:space-x-6 sm:space-y-0 space-y-4">
-            <a
-              className=" p-3 motion-safe:hover:scale-110 hover:bg-primary hover:text-white flex text-green-300 ring-green-300 ring-2 "
-              href="https://pepi.codes/resume.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Resume
-            </a>
-          </div>
+          <a
+            className="w-32 text-center p-3 motion-safe:hover:scale-110 hover:bg-primary hover:text-white text-green-300 ring-green-300 ring-2 "
+            href="https://pepi.codes/resume.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
         </Fade>
       </div>
     </div>
