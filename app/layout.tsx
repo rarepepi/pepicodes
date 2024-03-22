e0de87bcc71b94d171ef8803d22cc8f90465f918
rarepepi <pepimartinez@protonmail.com>
2024-03-22 03:38:47 -0400

    cloudflare testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="">
       <head>
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "4e7d79b944524a3980df696e96f933a6"}'
+        ></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
