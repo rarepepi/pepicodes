b2cef66479326768a0e77d24e2b06538b6223d02
rarepepi <pepimartinez@protonmail.com>
2024-09-04 15:52:03 -0400

    add umami tracking script for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          defer
+          src="https://umami.pepi.codes/script.js"
+          data-website-id="1ac4cbc1-7b53-4517-b1fb-1ceaaeafe5ab"
+        />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
