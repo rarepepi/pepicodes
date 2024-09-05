faabcbfafde38c86b4d60b52b3ec72a9739416ec
rarepepi <pepimartinez@protonmail.com>
2024-09-05 00:38:51 -0400

    fix date

--- data/resume.tsx
+++ data/resume.tsx
@@ -164,7 +164,7 @@ export const DATA = {
     {
       title: "Blockery",
       href: "https://blockery.app",
-      dates: "Jul 2022 - Mar 2024",
+      dates: "Jul 2022 - Mar 2023",
       active: true,
       description: "Your new home for NFT analytics and knowledge.",
       technologies: ["Typescript", "Node.js", "Nest.js", "PostgreSQL"],
